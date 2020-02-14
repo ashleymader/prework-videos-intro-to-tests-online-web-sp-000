20ec17bac62dad59067f404de9c7db75285ae713
Ashley M Mader <ashley.mader12@gmail.com>
2020-02-14 18:03:21 +0000

    Automatically backed up by Learn

--- conversions_test.rb
+++ conversions_test.rb
@@ -16,3 +16,11 @@ else
   puts "Test failed: got #{answer} instead"
 end
 
+ruby require_relative './conversions.rb' 
+puts "ounces_to_grams when given 0, returns 0.0" 
+answer = ounces_to_grams(0) 
+if answer == 0.0 
+    puts "Passed the test!" 
+else 
+    puts "Failed the test: got #{answer} instead" 
+end
